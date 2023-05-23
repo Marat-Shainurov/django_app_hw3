86e8535b9f31513ce8cbcd4bdc5d8a94cf32a056
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-23 18:45:39 +0300

    Contacts info's been added to the view and template

--- catalog/views.py
+++ catalog/views.py
@@ -2,12 +2,13 @@ import json
 
 from django.shortcuts import render
 
-from catalog.models import Product
+from catalog.models import Product, Contacts
 from catalog.utils import get_model_objects, get_last_five_products
 
 
 def index(request):
     data = get_model_objects(Product)
     last_five_products = get_last_five_products(data)
-    context = {'data_to_display': last_five_products}
+    contacts_data = get_model_objects(Contacts)
+    context = {'data_to_display': last_five_products, 'contacts': contacts_data[0]}
     return render(request, 'catalog/index.html', context)
