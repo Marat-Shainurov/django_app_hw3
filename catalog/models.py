08422e8fe58bfc4a370c45257a5defe14fa26b2a
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-23 14:50:02 +0300

    models update

--- catalog/models.py
+++ catalog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Product(models.Model):
-    name = models.CharField(null=False, max_length=50, verbose_name='product_name')
-    description = models.TextField(verbose_name='product_description')
-    img = models.ImageField(upload_to='media/', verbose_name='product_img')
-    category = models.CharField(null=False, max_length=50, verbose_name='product_category')
-    price = models.IntegerField(null=False, verbose_name='product_price')
-    creation_date = models.DateTimeField(verbose_name='product_creation_date')
-    last_change_date = models.DateTimeField(verbose_name='product_clast_change_date')
+    name = models.CharField(max_length=50, verbose_name='product_name')
+    description = models.TextField(verbose_name='product_description', **NULLABLE)
+    img = models.ImageField(upload_to='media/', verbose_name='product_img', **NULLABLE)
+    category = models.CharField(max_length=50, verbose_name='product_category')
+    price = models.IntegerField(verbose_name='product_price')
+    creation_date = models.DateTimeField(verbose_name='product_creation_date', **NULLABLE)
+    last_change_date = models.DateTimeField(verbose_name='product_clast_change_date', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} {self.category} {self.price}'
@@ -21,8 +23,7 @@ class Product(models.Model):
 
 class Category(models.Model):
     category_name = models.CharField(null=False, max_length=50, verbose_name='category_name')
-    category_description = models.TextField(null=False, verbose_name='category_description')
-
+    category_description = models.TextField(verbose_name='category_description', **NULLABLE)
 
     def __str__(self):
         return f'{self.category_name} {self.category_description}'
