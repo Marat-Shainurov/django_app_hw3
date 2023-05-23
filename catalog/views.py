e35be2a99169eba426c7d65b4727bc5d3f1d0356
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-23 17:37:01 +0300

    view's been added and set

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
+import json
+
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Product
+from catalog.utils import get_model_objects, get_last_five_products
+
+
+def index(request):
+    data = get_model_objects(Product)
+    last_five_products = get_last_five_products(data)
+    context = {'data_to_display': last_five_products}
+    return render(request, 'catalog/index.html', context)
